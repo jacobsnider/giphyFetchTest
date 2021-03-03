4cd0d45cabb400d77038995d56be10ee20d4d473
jacobsnider <jakesniderlmt@gmail.com>
2021-03-03 18:57:12 -0500

    branch 1 attempt

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// let random = random() => [~~(Math.random() * random().length)];
+// const fetch = require('node-fetch');
+
+const card = document.querySelector('.card');
+const img = card.querySelector('img');
+const btn = card.querySelector('button');
+var gifUrl;
+var gifTitle;
+
 
 const giphy = {
     baseURL: "https://api.giphy.com/v1/gifs/",
@@ -6,26 +13,56 @@ const giphy = {
     tag: `It's Always Sunny In Philadelphia`,
     // type: "random",
 };
-
+// API URL -> https://api.giphy.com/v1/gifs/search?q=It%27s%20Always%20Sunny%20In%20Philadelphia&api_key=dc6zaTOxFJmzC
 const url = `${giphy.baseURL}search?q=${giphy.tag}&api_key=${giphy.apiKey}`;
 
-fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        let giphyResults = json.data
-            // Target for giphyResults -> [0].images.fixed_height.url
-        let random = giphyResults[~~(Math.random() * giphyResults.length)];
-        console.log(random);
-        const resultsDiv = document.querySelector('.card')
-        let results = `
-        <img src="${random.images.fixed_height.url}" alt="Giphy Response">
-        <button style="color: #000;">GET NEW GIF</button>
-        `
-        resultsDiv.innerHTML = results;
-    });
-// .catch(err => console.log(err.message))
+// -------------------
+// FETCH FUNCTIONS -> fetch data from an api and parse the response to JSON
+// -------------------
 
+function fetchData(url) {
+    return fetch(url)
+        .then(res => res.json())
+}
+fetchData(url)
+    .then(json => {
+        gifUrl = generateGif(json.data);
+        displayGif(gifUrl);
+    })
 
 // -------------------
 // HELPER FUNCTIONS
 // -------------------
+// Recieve json & select random url from response array
+function generateGif(json) {
+    let giphyResults = json;
+    let random = giphyResults[~~(Math.random() * giphyResults.length)];
+    gifUrl = random.images.original.url;
+    gifTitle = random.title;
+    console.log(gifUrl);
+    return gifUrl;
+}
+
+function displayGif(gifUrl) {
+    const html = `
+                <img src="${gifUrl}" alt="${gifTitle}">
+                <button style="color: #000;">GET NEW GIF</button>
+                `;
+    card.innerHTML = html;
+}
+
+function fetchNewGif(gifUrl) {
+    var newGif = img.src(gifUrl);
+    fetchData(gifUrl)
+        .then(data => {
+            img.src = `${gifUrl}`;
+            img.alt = `${gifTitle}`;
+        })
+}
+
+// --------------- 
+// EVENT LISTENERS
+// ---------------
+
+// onSelect.addEventListener('change', fetchNewGif);
+// btn.addEventListener('click', fetchNewGif)
